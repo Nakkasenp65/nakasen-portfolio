d575f04ccce88ab525b5318e2740b74bb139903e
Nakkasenp65 <nakkasenputtacharoen@gmail.com>
2025-05-31 18:37:10 +0700

    refine width for sorter

--- src/component/Dropdown/Dropdown.jsx
+++ src/component/Dropdown/Dropdown.jsx
@@ -2,7 +2,7 @@
 import { useState } from "react";
 import s from "./Dropdown.module.css";
 import { GoTriangleDown } from "react-icons/go";
-export default function Dropdown({ item = "Date", setSortOrder, sortOrder }) {
+export default function Dropdown({ setSortOrder, sortOrder }) {
   const [isOpen, setIsOpen] = useState(true);
 
   return (
@@ -14,19 +14,20 @@ export default function Dropdown({ item = "Date", setSortOrder, sortOrder }) {
         >
           <div className={s.items}>
             <span className={s.item} onClick={() => setSortOrder("asc")}>
-              Asc
+              Sort by oldest
             </span>
             <hr className={s.line} />
             <span className={s.item} onClick={() => setSortOrder("desc")}>
-              Desc
+              Sort by newest
             </span>
           </div>
         </div>
       ) : (
         <div className={s.select} onClick={() => setIsOpen(!isOpen)}>
-          <div className={s.item}>Sort by {item}</div>
+          <div className={s.item}>
+            {sortOrder === "asc" ? "Sort by oldest" : "Sort by newest"}
+          </div>
           <div className={s.icon}>
-            {sortOrder}
             <GoTriangleDown
               className={`${s.triangle} ${isOpen ? s.active : null}`}
             />
